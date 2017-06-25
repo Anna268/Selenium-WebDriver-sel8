5f6f8037e727ae3bc5265207d6f3f18e4f6a1e2e
Anna268 <Chashkova_2011@mail.ru>
2017-06-26 00:36:53 +0500

    Задание 1

---
+++ Вебинар2.1/Вебинар2.1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Вебинар2._1
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "https://habrahabr.ru/post/152653/";
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
